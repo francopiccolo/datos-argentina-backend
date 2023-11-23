d66c081fcacc3d537accef9281005a98098594dc
Franco Piccolo <francopiccolo@gmail.com>
2023-11-23 23:39:57 +0300

    added order by year

--- sql/pct_ingresos_sobre_gasto.sql
+++ sql/pct_ingresos_sobre_gasto.sql
@@ -1,4 +1,5 @@
 SELECT EXTRACT(year FROM indice_tiempo) AS year,
        SUM(CASE WHEN prestacion_l1 = 'ingresos' THEN gasto ELSE 0 END) / SUM(gasto) AS pct_ingresos
 FROM fact.gasto_publico_anual_por_prestacion
-GROUP BY 1;
+GROUP BY 1
+ORDER BY 1;
